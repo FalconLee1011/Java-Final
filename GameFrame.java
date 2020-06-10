096a90230cdb9bb33adc80b06f655dce4011e62d
FalconLee1011 <falcon.lee1011@gmail.com>
2020-06-11 02:05:40 +0800

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -190,7 +190,7 @@ public class GameFrame extends JFrame {
                         for (int i = 0; i < brickList.size(); i++) {
                             r.nextInt(brickList.size() - 1);
                             Enery e = brickList.get(i);
-                            e.img = new ImageIcon("img/Back.png").getImage();
+                            e.img = new ImageIcon("img/teacher_downMove_GIF.gif").getImage();
                             // try { Thread.sleep(5); }
                             // catch (Exception e3) {}
                         }
