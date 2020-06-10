d924d592fe2e0034c7149dc28f3eae19c38daf5f
Popcorn0924-0924 <66465463+Popcorn0924-0924@users.noreply.github.com>
2020-06-11 00:15:48 +0800

    heart

---
+++ Heart.java
@@ -0,0 +1,16 @@
+package peekaboo.props;
+
+import java.awt.Image;
+
+/**
+ * 坑洞 地板被挖掉之後的地板
+ */
+public class Heart extends Enery {
+
+    public Heart(int x, int y, int width, int height, Image img) {
+
+        super(x, y, width, height, img);
+
+    }
+
+}
