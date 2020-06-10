f608cc410aaf5f3bf3dbe5467084cf4d46d7eb5c
FalconLee1011 <falcon.lee1011@gmail.com>
2020-06-11 00:23:34 +0800

    Create InitMap.java

---
+++ InitMap.java
@@ -0,0 +1,48 @@
+//地圖配置
+package peekaboo;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class InitMap {
+
+    // 數據容器
+    public ArrayList<String> list = new ArrayList<String>();
+    public int[][] map = null;
+
+    public int[][] readMap() throws Exception {// return 2D-array
+
+        // 構造文件輸入流
+        FileInputStream file = new FileInputStream("MAPS/map.txt");
+        InputStreamReader isr = new InputStreamReader(file);
+        BufferedReader br = new BufferedReader(isr);
+
+        String value = br.readLine();// 讀取一行數據
+        while (value != null) {// 還有內容
+            list.add(value);// 將讀取到的一行數據加入到容器中
+            value = br.readLine();
+        }
+        br.close();
+
+        int row = list.size();// 得到多少行多少列
+        int cloum = 0;
+        for (int i = 0; i < 1; i++) {
+            String str = list.get(i);// list[i]
+            String[] values = str.split(",");
+            cloum = values.length;// 1 row 幾個 elements
+        }
+        map = new int[row][cloum];
+        // 將讀到的字符創轉換成整數，並賦值給二位數組map
+        for (int i = 0; i < list.size(); i++) {
+            String str = list.get(i);
+            String[] values = str.split(",");
+            for (int j = 0; j < values.length; j++) {
+                map[i][j] = Integer.parseInt(values[j]);
+            }
+        }
+        return map;
+    }
+}
