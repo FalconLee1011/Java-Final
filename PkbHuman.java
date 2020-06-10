437069596e0a4bcadcd6361ebdf22f58158a0c72
Popcorn0924-0924 <66465463+Popcorn0924-0924@users.noreply.github.com>
2020-06-10 11:44:49 +0800

    人

--- PkbHuman.java
+++ PkbHuman.java
@@ -66,6 +66,25 @@ public class PkbHuman extends Thread {
                 yspeed = 8;
                 Toolkit.getDefaultToolkit().beep();
                 // timer.cancel();
+                // timer.purge();
+            }
+        };
+
+        timer.schedule(test, t);
+        run = false;
+    }
+
+    public void Timecamel(int t) {
+        TimerTask test = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("10秒到了");
+                isWitch = false;
+                camel = false;
+                xspeed = 8;
+                yspeed = 8;
+                Toolkit.getDefaultToolkit().beep();
+                // timer.cancel();
                 timer.purge();
             }
         };
@@ -105,24 +124,29 @@ public class PkbHuman extends Thread {
                     sequence = 2;
                     System.out.println("shoe");
                     Time(12000);
+
                 } else if (bumpedEnery instanceof Turtle) {
                     xspeed = 2;
                     yspeed = 2;
                     Time(10000);
+
                 } else if (bumpedEnery instanceof Stone) {
                     // 若道具是 Stone (pipe) 則放入背包
                     backpack.add(bumpedEnery);
                 } else if (bumpedEnery instanceof Fruit) {
-                    img = new ImageIcon("img/downMove_GIF.gif").getImage();
+                    img = new ImageIcon("img/camelHuman_downMove_GIF.gif").getImage();
                     teacher = true;
                     sequence = 6;
 
                     Time(10000);
+
                     // backpack.add(bumpedEnery);
                 } else if (bumpedEnery instanceof Bewitch) {
+
                     img = new ImageIcon("img/downMove_GIF.gif").getImage();
                     isWitch = true;
                     Time(10000);
+
                     // backpack.add(bumpedEnery);
                 }
                 // TODO: 其他道具
@@ -149,7 +173,7 @@ public class PkbHuman extends Thread {
             // && bump(gameFrame.rockList,Str_Up)==0){//碰觸到道具，道具不影響速度變0 this.yspeed = 0; }
             if (teacher == true && camel == true) {
                 if (sequence == 6)
-                    this.img = new ImageIcon("img/human_upMove_gif_160.gif").getImage();
+                    this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
                 else if (sequence == 2)
                     this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
             } else if (teacher == false && camel == true)
@@ -158,7 +182,7 @@ public class PkbHuman extends Thread {
                 this.img = new ImageIcon("img/human_upMove_gif_160.gif").getImage();
 
             else if (teacher == true && camel == false)
-                this.img = new ImageIcon("img/upMove_GIF.gif").getImage();
+                this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
 
             this.lastDirection = Str_Up;
             if (this.y >= 0 && this.y <= 300) {
@@ -187,7 +211,7 @@ public class PkbHuman extends Thread {
 
             if (teacher == true && camel == true) {
                 if (sequence == 6)
-                    this.img = new ImageIcon("img/human_downMove_gif_160.gif").getImage();
+                    this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
                 else if (sequence == 2)
                     this.img = new ImageIcon("img/camelHuman_downMove_GIF.gif").getImage();
             } else if (teacher == false && camel == true)
@@ -223,11 +247,11 @@ public class PkbHuman extends Thread {
 
             if (teacher == true && camel == true) {
                 if (sequence == 6)
-                    this.img = new ImageIcon("img/human_leftMove_gif_160.gif").getImage();
+                    this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
                 else if (sequence == 2)
-                    this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
+                    this.img = new ImageIcon("img/camelHuman_leftMove_GIF.gif").getImage();
             } else if (teacher == false && camel == true)
-                this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
+                this.img = new ImageIcon("img/camelHuman_leftMove_GIF.gif").getImage();
             else if (teacher == false && camel == false)
                 this.img = new ImageIcon("img/human_leftMove_gif_160.gif").getImage();
 
@@ -259,11 +283,11 @@ public class PkbHuman extends Thread {
 
             if (teacher == true && camel == true) {
                 if (sequence == 6)
-                    this.img = new ImageIcon("img/human_rightMove_gif_160.gif").getImage();
+                    this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
                 else if (sequence == 2)
-                    this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
+                    this.img = new ImageIcon("img/camelHuman_rightMove_GIF.gif").getImage();
             } else if (teacher == false && camel == true)
-                this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
+                this.img = new ImageIcon("img/camelHuman_rightMove_GIF.gif").getImage();
             else if (teacher == false && camel == false)
                 this.img = new ImageIcon("img/human_rightMove_gif_160.gif").getImage();
 
@@ -322,13 +346,13 @@ public class PkbHuman extends Thread {
             // && bump(gameFrame.rockList,Str_Up)==0){//碰觸到道具，道具不影響速度變0 this.yspeed = 0; }
             if (teacher == true && camel == true) {
                 if (sequence == 6)
-                    this.img = new ImageIcon("img/human_downMove_gif_160.gif").getImage();
+                    this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
                 else if (sequence == 2)
-                    this.img = new ImageIcon("img/camelHuman_downMove_GIF.gif").getImage();
+                    this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
             } else if (teacher == false && camel == true)
-                this.img = new ImageIcon("img/camelHuman_downMove_GIF.gif").getImage();
+                this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
             else if (teacher == false && camel == false)
-                this.img = new ImageIcon("img/human_downMove_gif_160.gif").getImage();
+                this.img = new ImageIcon("img/human_upMove_gif_160.gif").getImage();
 
             else if (teacher == true && camel == false)
                 this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
@@ -357,13 +381,13 @@ public class PkbHuman extends Thread {
 
             if (teacher == true && camel == true) {
                 if (sequence == 6)
-                    this.img = new ImageIcon("img/human_upMove_gif_160.gif").getImage();
+                    this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
                 else if (sequence == 2)
-                    this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
+                    this.img = new ImageIcon("img/camelHuman_downMove_GIF.gif").getImage();
             } else if (teacher == false && camel == true)
-                this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
+                this.img = new ImageIcon("img/camelHuman_downMove_GIF.gif").getImage();
             else if (teacher == false && camel == false)
-                this.img = new ImageIcon("img/human_upMove_gif_160.gif").getImage();
+                this.img = new ImageIcon("img/human_downMove_gif_160.gif").getImage();
 
             else if (teacher == true && camel == false)
                 this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
@@ -393,13 +417,13 @@ public class PkbHuman extends Thread {
 
             if (teacher == true && camel == true) {
                 if (sequence == 6)
-                    this.img = new ImageIcon("img/human_rightMove_gif_160.gif").getImage();
+                    this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
                 else if (sequence == 2)
-                    this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
+                    this.img = new ImageIcon("img/camelHuman_leftMove_GIF.gif").getImage();
             } else if (teacher == false && camel == true)
-                this.img = new ImageIcon("img/camelHuman_upMove_GIF.gif").getImage();
+                this.img = new ImageIcon("img/camelHuman_leftMove_GIF.gif").getImage();
             else if (teacher == false && camel == false)
-                this.img = new ImageIcon("img/human_rightMove_gif_160.gif").getImage();
+                this.img = new ImageIcon("img/human_leftMove_gif_160.gif").getImage();
 
             else if (teacher == true && camel == false)
                 this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
@@ -429,13 +453,13 @@ public class PkbHuman extends Thread {
 
             if (teacher == true && camel == true) {
                 if (sequence == 6)
-                    this.img = new ImageIcon("img/human_leftMove_gif_160.gif").getImage();
+                    this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
                 else if (sequence == 2)
-                    this.img = new ImageIcon("img/camelHuman_downMove_GIF.gif").getImage();
+                    this.img = new ImageIcon("img/camelHuman_rightMove_GIF.gif").getImage();
             } else if (teacher == false && camel == true)
-                this.img = new ImageIcon("img/camelHuman_downMove_GIF.gif").getImage();
+                this.img = new ImageIcon("img/camelHuman_rightMove_GIF.gif").getImage();
             else if (teacher == false && camel == false)
-                this.img = new ImageIcon("img/human_leftMove_gif_160.gif").getImage();
+                this.img = new ImageIcon("img/human_rightMove_gif_160.gif").getImage();
 
             else if (teacher == true && camel == false)
                 this.img = new ImageIcon("img/downMove_GIF.gif").getImage();
