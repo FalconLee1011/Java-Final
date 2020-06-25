b54e9e21690bf5ae94605c1162a60608f69b450e
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-06-25 17:22:36 +0800

    0625

--- Enery.java
+++ Enery.java
@@ -8,13 +8,13 @@ import java.awt.Image;
 public abstract class Enery {
 
     public int x, y;// 位置
-    public int raw_x, raw_y;// 分別對應到map[i][j]的i,j
+    public int arrRow, arrCol;// 分別對應到map[i][j]的i,j
     public int width, height;
     public Image img;
 
     public Enery(int x, int y, int width, int height, Image img) {
-        this.raw_x = y / 120;
-        this.raw_y = x / 120;
+        this.arrRow = y / 120;
+        this.arrCol = x / 120;
         this.x = x;
         this.y = y;
         this.width = width;
